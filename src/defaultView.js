dc06c003e069ca183ca323d0adb8cf2eb021e004
dodo-coding-failures <damianek926@gmail.com>
2024-08-09 23:12:22 +0200

    deploy freddy fazbear
    added default layout,  also  music works again (yes, I did it AGAIN)

---
+++ src/defaultView.js
@@ -0,0 +1,23 @@
+document.getElementById('resetView').addEventListener('click', ()=>{
+    let el = document.getElementById('debug');
+    el.style.position = 'absolute'
+    el.style.top = (window.innerHeight-el.offsetHeight-25)+'px';
+    el.style.left = '0px';
+
+    el = document.getElementById('track');
+    el.style.position = 'absolute'
+    el.style.top = (window.innerHeight-el.offsetHeight-50)+'px';
+    el.style.left = (window.innerWidth-el.offsetWidth)+'px';
+
+    el = document.getElementById('search');
+    el.style.position = 'absolute'
+    el.style.top = '10px';
+    el.style.left = (window.innerWidth/2-el.offsetWidth/2)+'px';
+
+    el = document.getElementById('shortcuts');
+    el.style.position = 'absolute'
+    el.style.top = (document.getElementById('search').offsetHeight+20)+'px';
+    el.style.left = (window.innerWidth/2-el.offsetWidth/2)+'px';
+
+});
+
